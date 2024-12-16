d92c230105d45292499be9f8f1ad75bddbd2c784
Joki <josedavidamayar@gmail.com>
2024-12-16 18:25:19 -0500

    feat: add routes nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-const App = () => {
-  return <div className="bg-red-400 text-white">Tailwind is working!</div>;
-};
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginDashboard from "./components/LoginDashboard";
+import UserDashboard from "./components/UserDashboard";
 
-export default App;
+/**
+ * Private route for the user dashborad
+ * @param param0 the jsx element
+ * @returns the route to navigate
+ */
+function PrivateRouteUser({ children }: { children: JSX.Element }) {
+  const userId = localStorage.getItem("userId");
+  return userId ? children : <Navigate to={"/login"} />;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginDashboard />} />
+          <Route
+            path="/user-dashboard"
+            element={
+              <PrivateRouteUser>
+                <UserDashboard />
+              </PrivateRouteUser>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
