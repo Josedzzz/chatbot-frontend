cd984c825abb7ac5c4fbca2c73fc856a6a21f2d2
Joki <josedavidamayar@gmail.com>
2024-12-16 18:26:23 -0500

    feat: add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-white": "#F0F0F0",
+        "custom-black": "#0F0F0F",
+        "custom-beige": "#FFF3E3",
+      }
+    },
   },
   plugins: [],
 };
