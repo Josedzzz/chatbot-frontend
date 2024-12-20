fc47427c94a32af0c2877c4c9bbe283535dadded
Joki <josedavidamayar@gmail.com>
2024-12-20 00:50:02 -0500

    refactor: just format the config code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "custom-white": "#F0F0F0",
         "custom-black": "#0F0F0F",
         "custom-beige": "#FFF3E3",
-      }
+      },
     },
   },
   plugins: [],
 };
-
-
