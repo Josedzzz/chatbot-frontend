588305225245805565b4cc8ff015f5edbb46fae6
Joki <josedavidamayar@gmail.com>
2024-12-24 19:32:07 -0500

    feat: create the component for the user chat

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,5 +1,73 @@
-export default function UserDashboard() {
+import { useEffect, useState } from "react";
+import {
+  ChatHistoryData,
+  chatService,
+  loadChatHistoryService,
+} from "../services/chatService";
+import UserHeader from "./UserHeader";
+
+interface UserDashboardProps {
+  userId: string;
+}
+
+export default function UserDashboard({ userId }: UserDashboardProps) {
+  const [chatHistory, setChatHistory] = useState<ChatHistoryData | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [newMessage, setNewMessage] = useState<string>("");
+
+  /**
+   * Gets the user's chat history
+   */
+  const loadChatHistory = async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await loadChatHistoryService(userId);
+      if (response.success) {
+        setChatHistory(response.data);
+      } else {
+        setError(response.error.message || "Failed to load chat history");
+      }
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  /**
+   * Send a message and load the chat again
+   * @returns
+   */
+  const sendMessage = async () => {
+    if (newMessage.trim()) return;
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await chatService({ userId, prompt: newMessage });
+      if (response.success) {
+        setNewMessage("");
+        await loadChatHistory();
+      } else {
+        setError(response.error.message || "Failed to send the message");
+      }
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadChatHistory();
+  });
+
   return (
-    <h1 className="font-extrabold">UserDashboard component</h1>
-  )
+    <div className="min-h-screen bg-custom-beige flex flex-col">
+      <div className="p-3">
+        <UserHeader title={chatHistory?.title || "Loading chat..."} />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/UserHeader.tsx
@@ -0,0 +1,19 @@
+interface UserHeaderProps {
+  title: string;
+}
+
+export default function UserHeader({ title }: UserHeaderProps) {
+  return (
+    <header className="h-16 w-full rounded-xl bg-custom-white border-4 border-custom-black flex justify-between">
+      <div className="flex items-center space-x-1 gap-3">
+        <h1 className="text-xl sm:text-3xl md:text-4xl font-bold text-custom-black">
+          Joki Chat
+        </h1>
+        <i className="fa-solid fa-robot text-xl sm:text-3xl md:text-4xl text-custom-black"></i>
+      </div>
+      <h2 className="text-xl sm:text-3xl font-bold text-custom-black">
+        {title}
+      </h2>
+    </header>
+  );
+}
