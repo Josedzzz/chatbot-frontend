32084964a71c772febaab10939d80a0f0f5b248d
Joki <josedavidamayar@gmail.com>
2024-12-31 02:12:46 -0500

    feat: add logout button in the user header

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,19 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 interface UserHeaderProps {
   title: string;
 }
 
 export default function UserHeader({ title }: UserHeaderProps) {
+  const navigate = useNavigate();
+
   return (
     <header className="h-16 p-4 w-full rounded-xl bg-custom-white border-4 border-custom-black flex justify-between items-center">
       <div className="flex items-center space-x-1 gap-3">
         <h1 className="text-xl sm:text-3xl md:text-4xl font-bold text-custom-black">
-          Joki Chat
+          {title}
         </h1>
         <i className="fa-solid fa-robot text-xl sm:text-3xl md:text-4xl text-custom-black"></i>
       </div>
-      <h2 className="text-xl sm:text-3xl font-bold text-custom-black">
-        {title}
-      </h2>
+      <div className="flex items-center gap-3 hover:scale-110 transition duration-300 ease-in-out">
+        <h2 className="text-xl font-bold text-custom-black">Log out</h2>
+        <i
+          onClick={() => navigate("/login")}
+          className="fa-solid fa-arrow-right-from-bracket text-xl "
+        ></i>
+      </div>
     </header>
   );
 }
